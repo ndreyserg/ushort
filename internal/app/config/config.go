00f8f51fcf119da2acdb66761c3c35e9828e3bae
ndreyserg <ndreyserg@gmail.com>
2025-03-16 15:36:28 +0500

    json file config

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,31 +2,35 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 )
 
 // Config структура основных настроек.
 type Config struct {
-	ServerAddr  string // Адрес сервера.
-	BaseURL     string // Адрес сервера для перенаправления.
-	LogLevel    string // Уровень логирования.
-	StoragePath string // Путь к файлу для хранения в файле.
-	DSN         string // Строка подключения к БД.
-	Secret      string // Строка с секрета для JWT.
-	EnableHTTPS bool   // Строка с секрета для JWT.
+	ServerAddr  string `json:"server_address"`    // Адрес сервера.
+	BaseURL     string `json:"base_url"`          // Адрес сервера для перенаправления.
+	LogLevel    string `json:"info"`              // Уровень логирования.
+	StoragePath string `json:"file_storage_path"` // Путь к файлу для хранения в файле.
+	DSN         string `json:"database_dsn"`      // Строка подключения к БД.
+	Secret      string `json:"secret"`            // Строка с секрета для JWT.
+	EnableHTTPS bool   `json:"enable_https"`      // Использовать https.
 }
 
 // MakeConfig возвращает структуру с настройками.
 func MakeConfig() Config {
-	con := Config{}
-	flag.StringVar(&con.ServerAddr, "a", "localhost:8080", "server address")
-	flag.StringVar(&con.BaseURL, "b", "http://localhost:8080", "result base url")
-	flag.StringVar(&con.LogLevel, "l", "info", "log level")
-	flag.StringVar(&con.StoragePath, "f", "", "storage file path")
-	flag.StringVar(&con.DSN, "d", "", "DSN")
-	flag.StringVar(&con.Secret, "k", "secret_key", "secret key")
-	flag.BoolVar(&con.EnableHTTPS, "s", false, "Enable HTTPS")
+	con := getDefaultConfig()
+	flag.String("c", "", "config file path")
+	flag.String("сonfig", "", "config file path")
+	flag.StringVar(&con.ServerAddr, "a", con.ServerAddr, "server address")
+	flag.StringVar(&con.BaseURL, "b", con.BaseURL, "result base url")
+	flag.StringVar(&con.LogLevel, "l", con.LogLevel, "log level")
+	flag.StringVar(&con.StoragePath, "f", con.StoragePath, "storage file path")
+	flag.StringVar(&con.DSN, "d", con.DSN, "DSN")
+	flag.StringVar(&con.Secret, "k", con.Secret, "secret key")
+	flag.BoolVar(&con.EnableHTTPS, "s", con.EnableHTTPS, "Enable HTTPS")
 	flag.Parse()
 
 	if envServerAddr := os.Getenv("SERVER_ADDRESS"); envServerAddr != "" {
@@ -58,4 +62,47 @@ func MakeConfig() Config {
 	}
 
 	return con
+}
+
+func getDefaultConfig() Config {
+
+	conf := Config{
+		ServerAddr: "localhost:8080",
+		BaseURL:    "http://localhost:8080",
+		LogLevel:   "info",
+		Secret:     "secret_key",
+	}
+	confPath := ""
+	confPathIndex := 0
+	for i, v := range os.Args[1:] {
+		if v == "-c" || v == "-config" {
+			confPathIndex = i + 1
+		}
+		if i == confPathIndex {
+			confPath = v
+		}
+	}
+
+	if envPath := os.Getenv("CONFIG"); envPath != "" {
+		confPath = envPath
+	}
+
+	if confPath == "" {
+		return conf
+	}
+
+	file, err := os.OpenFile(confPath, os.O_RDONLY, 0666)
+
+	if err != nil {
+		fmt.Println("Ошибка открытия файла конфигурации")
+		return conf
+	}
+
+	dec := json.NewDecoder(file)
+	err = dec.Decode(&conf)
+	fmt.Println(conf)
+	if err != nil {
+		fmt.Println("Ошибка декодирования файла конфигурации")
+	}
+	return conf
 }
